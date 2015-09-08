625e6ea8d96628a72902f62deecafbbdc018356b
Henri Koski <henri.t.koski@gmail.com>
2015-09-09 01:45:05 +0300

    Some testing

---
+++ client.go
@@ -0,0 +1,94 @@
+package girc
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	conn     *net.TCPConn
+	server   string
+	nick     string
+	realName string
+	rx       chan string
+	tx       chan string
+	quit     chan bool
+}
+
+func NewClient(s string, n string, r string) (*Client, error) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", s)
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	rx := make(chan string)
+	tx := make(chan string)
+	q := make(chan bool)
+
+	newClient := Client{c, s, n, r, rx, tx, q}
+
+	go newClient.handleMessageFromServer()
+	go newClient.listenServer()
+
+	return &newClient, nil
+}
+
+func (c *Client) listenServer() {
+	for {
+		message, err := bufio.NewReader(c.conn).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			c.Quit()
+			break
+		}
+		c.rx <- message
+	}
+}
+
+func (c *Client) handleMessageFromServer() {
+L:
+	for {
+		select {
+		case msg := <-c.rx:
+			fmt.Printf("SERVER: %s", msg)
+		case <-c.quit:
+			fmt.Println("Closing client")
+			break L
+		}
+	}
+	c.quit <- true
+}
+
+func (c *Client) Connect() {
+	fmt.Println("CONNECT")
+	c.write("PASS *")
+	c.write("NICK heppu_girc")
+	c.write("USER heppu_girc 0 * :Go Botti")
+}
+
+func (c *Client) Join() {
+	fmt.Println("JOIN")
+	c.write("JOIN #gobot")
+}
+
+func (c *Client) Send() {
+	fmt.Println("PRIVMSG")
+	c.write("PRIVMSG #gobot hello")
+}
+
+func (c *Client) Quit() {
+	fmt.Println("QUIT")
+	c.quit <- true
+	<-c.quit
+	return
+}
+
+func (c *Client) write(msg string) {
+	fmt.Fprintf(c.conn, "%s\r\n", msg)
+}
---
+++ example/example.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	. "github.com/heppu/girc"
+	"os"
+)
+
+func main() {
+	// Config
+	s := "irc.atw-inter.net:6667"
+	n := "heppu_girc"
+	r := "Heppu Girc"
+
+	// Client
+	c, err := NewClient(s, n, r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+
+		switch text {
+		case "c\n":
+			c.Connect()
+		case "j\n":
+			c.Join()
+		case "h\n":
+			c.Send()
+		default:
+			c.Quit()
+			return
+		}
+	}
+	fmt.Println("Done")
+}
